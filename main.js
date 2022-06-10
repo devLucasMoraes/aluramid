ecc2f15438f3964927d7dfe7df9a927c86038d6b
devLucasMoraes <devlucasmoraes@gmai.com>
2022-06-10 19:17:34 -0300

    loop contador

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play()
-}
 
 const listaDeTeclas = document.querySelectorAll('.tecla')
 
+let contador = 0
+
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador]
+    const instrumento = tecla.classList[1]
+
+    const idSom = `#som_${instrumento}`
+    tecla.onclick = function () {
+        tocaSom(idSom)
+    }
+    contador++
+}
+
+function tocaSom (classSom) {
+    document.querySelector(classSom).play()
+}
